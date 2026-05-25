e5938cd8c49123873ec3c2c9d1e228c8b4f4b971
Filipe <filipe.lopes@aluno.ifsertao-pe.edu.br>
2026-05-25 00:23:37 -0300

    a

--- Projeto/Estoque_Vacinas.php
+++ Projeto/Estoque_Vacinas.php
@@ -11,7 +11,7 @@ require_once "sessao.php";
       email: <?php echo json_encode($_SESSION['usuario_email']); ?>,
       tipo:  <?php echo json_encode($_SESSION['usuario_tipo']); ?>
     };
-  </script>
+  </script> 
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Estoque Vacinas/Medicamentos - Vacinação Animal</title>
